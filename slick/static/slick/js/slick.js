02850bb1a9694b1095945c05f97bc0c6a416d636
Sjoerd Arendsen <subscribe@optixdesigns.com>
2014-11-03 11:13:02 +0100

    Last nights commit

--- slick/static/slick/js/slick.js
+++ slick/static/slick/js/slick.js
@@ -1,6 +1,28 @@
+// slick jQuery namespace
+var slk = {
+	"jQuery": jQuery.noConflict(true)
+};
+
+// django jQuery namespace
+var django = {
+	"jQuery": slk.jQuery.noConflict(true)
+};
+
 (function($) {
-    $('#sidebar [data-scrollbar="true"]').slimScroll({
-        height: '100%',
-        width: 'auto'
-    });
-}(jQuery));
+	$('#sidebar [data-scrollbar="true"]').slimScroll({
+		height: '100%',
+		width: 'auto'
+	});
+
+	slick.initFilter = function() {
+		// Dont autoclose
+		$('.dropdown-menu-form').click(function (e) {
+			e.stopPropagation();
+		});
+
+		$(".filter-choice").change(function() {
+			location.href = $(this).val();
+		});
+	};
+
+})(slk.jQuery);
