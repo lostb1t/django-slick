c80731da8ac0c48b8b872192cdb01952cd4f31a6
Sjoerd Arendsen <subscribe@optixdesigns.com>
2014-11-04 08:53:30 +0100

    More work

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name='django-admin',
+    name='django-slick',
     version='0.1',
     long_description=read('README.txt'),
     author='Sjoerd Arendsen',
